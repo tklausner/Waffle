fa106032027b8ef476b5221cedc588b777a5b76c
Ethan Wood-Thomas <ethankwthomas@gmail.com>
2020-05-04 22:55:00 -0400

    explore layout

--- src/screens/ExploreScreen.js
+++ src/screens/ExploreScreen.js
@@ -1,54 +1,123 @@
 import React, { Component } from "react";
 import { Container, Content, Body, Text, Button} from "native-base";
-import { Image, TouchableOpacity, StyleSheet} from "react-native"
+import { Image, TouchableOpacity, StyleSheet, Dimensions} from "react-native"
 import { Col, Row, Grid } from 'react-native-easy-grid';
+
+const windowWidth = Dimensions.get('window').width;
+const bigWidth = windowWidth*2/3;
+const littleWidth = windowWidth/3;
 
 export default class ExploreScreen extends Component {
   render() {
     return (
-      <Container>
+      <Container style = {styles.container}>
         <Content>
           <Grid>
           <Row>
             <Col style = {{width: "33.3333%"}}>
-              <Row style={{ backgroundColor: '#635DB7', aspectRatio: 1 }}>
-              <Button style = {{aspectRatio: 1, width: "100%"}}><Text>teddy</Text></Button>
+              <Row style={styles.item}>
+                <TouchableOpacity>
+                  <Image style = {styles.littleImage} source={require("../../assets/images/bing.png")}/>
+                </TouchableOpacity>
               </Row>
-              <Row style={{ aspectRatio: 1, backgroundColor: '#935DB7'}}>
+              <Row style={styles.item}>
+                <TouchableOpacity>
+                  <Image style = {styles.littleImage} source={require("../../assets/images/roor.jpeg")}/>
+                </TouchableOpacity>
               </Row>
             </Col>
             <Col style={styles.item}>
-            <TouchableOpacity>
-              <Image style = {styles.image} source={require("../../assets/images/ethan.jpeg")}/>
-            </TouchableOpacity>
+              <TouchableOpacity>
+                <Image style = {styles.bigImage} source={require("../../assets/images/ethan.jpeg")}/>
+              </TouchableOpacity>
             </Col>
           </Row>
           <Row>
-            <Col style={{ backgroundColor: '#135DB7', aspectRatio: 1 }}></Col>
-            <Col style={{ backgroundColor: '#235DB7', aspectRatio: 1 }}></Col>
-            <Col style={{ backgroundColor: '#335DB7', aspectRatio: 1 }}></Col>
+            <Col style={styles.item}>
+              <TouchableOpacity>
+                <Image style = {styles.littleImage} source={require("../../assets/images/ethan.jpeg")}/>
+              </TouchableOpacity>
+            </Col>
+            <Col style={styles.item}>
+              <TouchableOpacity>
+                <Image style = {styles.littleImage} source={require("../../assets/images/ethan.jpeg")}/>
+              </TouchableOpacity>
+            </Col>
+            <Col style={styles.item}>
+              <TouchableOpacity>
+                <Image style = {styles.littleImage} source={require("../../assets/images/ethan.jpeg")}/>
+              </TouchableOpacity>
+            </Col>
           </Row>
           <Row>
-            <Col style={{ backgroundColor: '#535DB7', aspectRatio: 1 }}></Col>
-            <Col style={{ backgroundColor: '#735DB7', aspectRatio: 1 }}></Col>
-            <Col style={{ backgroundColor: '#835DB7', aspectRatio: 1 }}></Col>
+            <Col style={styles.item}>
+              <TouchableOpacity>
+                <Image style = {styles.littleImage} source={require("../../assets/images/ethan.jpeg")}/>
+              </TouchableOpacity>
+            </Col>
+            <Col style={styles.item}>
+              <TouchableOpacity>
+                <Image style = {styles.littleImage} source={require("../../assets/images/ethan.jpeg")}/>
+              </TouchableOpacity>
+            </Col>
+            <Col style={styles.item}>
+              <TouchableOpacity>
+                <Image style = {styles.littleImage} source={require("../../assets/images/ethan.jpeg")}/>
+              </TouchableOpacity>
+            </Col>
           </Row>
           <Row>
-            <Col style={{ backgroundColor: '#435DB7'}}></Col>
+            <Col style={styles.item}>
+              <TouchableOpacity>
+                <Image style = {styles.bigImage} source={require("../../assets/images/teddy.png")}/>
+              </TouchableOpacity>
+            </Col>
             <Col style = {{width: "33.3333%"}}>
-              <Row style={{ backgroundColor: '#635DB7', aspectRatio: 1 }}></Row>
-              <Row style={{ backgroundColor: '#935DB7', aspectRatio: 1 }}></Row>
+              <Row style={styles.item}>
+                <TouchableOpacity>
+                  <Image style = {styles.littleImage} source={require("../../assets/images/ethan.jpeg")}/>
+                </TouchableOpacity>
+              </Row>
+              <Row style={styles.item}>
+                <TouchableOpacity>
+                  <Image style = {styles.littleImage} source={require("../../assets/images/ethan.jpeg")}/>
+                </TouchableOpacity>
+              </Row>
             </Col>
           </Row>
           <Row>
-            <Col style={{ backgroundColor: '#135DB7', aspectRatio: 1 }}></Col>
-            <Col style={{ backgroundColor: '#235DB7', aspectRatio: 1 }}></Col>
-            <Col style={{ backgroundColor: '#335DB7', aspectRatio: 1 }}></Col>
+            <Col style={styles.item}>
+              <TouchableOpacity>
+                <Image style = {styles.littleImage} source={require("../../assets/images/ethan.jpeg")}/>
+              </TouchableOpacity>
+            </Col>
+            <Col style={styles.item}>
+              <TouchableOpacity>
+                <Image style = {styles.littleImage} source={require("../../assets/images/ethan.jpeg")}/>
+              </TouchableOpacity>
+            </Col>
+            <Col style={styles.item}>
+              <TouchableOpacity>
+                <Image style = {styles.littleImage} source={require("../../assets/images/ethan.jpeg")}/>
+              </TouchableOpacity>
+            </Col>
           </Row>
           <Row>
-            <Col style={{ backgroundColor: '#535DB7', aspectRatio: 1 }}></Col>
-            <Col style={{ backgroundColor: '#735DB7', aspectRatio: 1 }}></Col>
-            <Col style={{ backgroundColor: '#835DB7', aspectRatio: 1 }}></Col>
+            <Col style={styles.item}>
+              <TouchableOpacity>
+                <Image style = {styles.littleImage} source={require("../../assets/images/ethan.jpeg")}/>
+              </TouchableOpacity>
+            </Col>
+            <Col style={styles.item}>
+              <TouchableOpacity>
+                <Image style = {styles.littleImage} source={require("../../assets/images/ethan.jpeg")}/>
+              </TouchableOpacity>
+            </Col>
+            <Col style={styles.item}>
+              <TouchableOpacity>
+                <Image style = {styles.littleImage} source={require("../../assets/images/ethan.jpeg")}/>
+              </TouchableOpacity>
+            </Col>
           </Row>
           </Grid>
         </Content>
@@ -61,21 +130,32 @@ module.export = ExploreScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue'
+    backgroundColor: "#ddd"
   },
 
   item: {
-      flex:1,
        backgroundColor:"#ddd",
        alignItems:'center',
        aspectRatio:1,
-       flexDirection: 'row'
+       justifyContent: 'center',
+       flex:1
   },
 
-  image: {
-      alignItems:'center',
+  bigImage: {
+       alignItems:'center',
        justifyContent:'center',
        flex:1,
-       resizeMode: 'cover'
+       width: bigWidth,
+       borderWidth: 0.5,
+       borderColor: "#ddd"
+  },
+
+  littleImage: {
+       alignItems:'center',
+       justifyContent:'center',
+       flex:1,
+       width: littleWidth,
+       borderWidth: 0.5,
+       borderColor: "#ddd"
   }
 })
