aa5b423d96297bb8f08800bb46e9ed3ffe7337fb
Ethan Wood-Thomas <ethankwthomas@gmail.com>
2020-05-22 15:14:48 -0400

    Update App.js

--- App.js
+++ App.js
@@ -7,8 +7,6 @@ import { Ionicons } from "@expo/vector-icons";
 import { createStore } from "redux";
 
 import RouteStack from "./src/routes/RouteStack";
-import LoginScreen from "./src/screens/LoginScreen";
-import WelcomeScreen from "./src/screens/WelcomeScreen";
 import rootReducer from "./src/store/reducers/rootReducer";
 import { Provider } from "react-redux";
 
@@ -38,7 +36,7 @@ export default class App extends React.Component {
     }
     return (
       <Provider store={store}>
-        <WelcomeScreen />
+        <RouteStack />
       </Provider>
     );
   }
