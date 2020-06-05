e856b2a033d14a4fe89090c749d1a4209f952fd6
Ethan Wood-Thomas <ethankwthomas@gmail.com>
2020-06-05 16:05:40 -0400

    yuh

--- src/components/headers/ExploreHeader.js
+++ src/components/headers/ExploreHeader.js
@@ -11,35 +11,38 @@ import {
   Button,
   Text,
 } from "native-base";
-import { TouchableWithoutFeedback, Keyboard } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-
+import { Image } from "react-native";
 import styles from "../../styles";
 import { MaterialIcons } from "@expo/vector-icons";
+import WaffleIcon from "../../../assets/images/OnlineLogo.png";
 
 export function ExploreHeader() {
   const navigation = useNavigation();
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <Header searchBar rounded>
-        <Item
-          regular
-          style={{
-            borderRadius: 50,
-            marginLeft: 10,
-            marginBottom: 5,
-            height: 35,
-          }}
-        >
-          <Icon name="ios-search" />
-          <Input placeholder="Search" />
-          <Icon name="ios-people" />
-        </Item>
-        <Button transparent style={{ marginBottom: 5 }}>
-          <Text>Cancel</Text>
-        </Button>
-      </Header>
-    </TouchableWithoutFeedback>
+    <Header searchBar rounded>
+      <Item
+        regular
+        style={{
+          borderRadius: 50,
+          marginLeft: 10,
+          marginBottom: 5,
+          height: 35,
+        }}
+      >
+        <Icon name="ios-search" />
+        <Input placeholder="Search" />
+      </Item>
+      <Image
+        source={WaffleIcon}
+        style={{
+          height: "80%",
+          width: "10%",
+          flex: 0,
+          marginLeft: "2%",
+        }}
+      />
+    </Header>
   );
 }
 module.export = ExploreHeader;
